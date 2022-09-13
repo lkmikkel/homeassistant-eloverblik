73bf57b4b7d4417acb94b138c328dfc0c6a96a74
Lasse K. Mikkelsen <lasse@klok-mikkelsen.dk>
2022-09-13 18:13:11 +0200

    sensor.py: move away from hour entities to rawdata attribute

--- custom_components/eloverblik/sensor.py
+++ custom_components/eloverblik/sensor.py
@@ -1,5 +1,6 @@
 """Platform for Eloverblik sensor integration."""
 import logging
+from datetime import datetime, timedelta
 from homeassistant.const import ENERGY_KILO_WATT_HOUR
 from homeassistant.helpers.entity import Entity
 from pyeloverblik.eloverblik import Eloverblik
@@ -16,27 +17,22 @@ async def async_setup_entry(hass, config, async_add_entities):
 
     sensors = []
     sensors.append(EloverblikEnergy("Eloverblik Energy Total", 'total', eloverblik))
-    for x in range(1, 25):
-        sensors.append(EloverblikEnergy(f"Eloverblik Energy {x-1}-{x}", 'hour', eloverblik, x))
     async_add_entities(sensors)
 
 
 class EloverblikEnergy(Entity):
     """Representation of a Sensor."""
 
-    def __init__(self, name, sensor_type, client, hour=None):
+    def __init__(self, name, sensor_type, client):
         """Initialize the sensor."""
+        self._rawdata = None
         self._state = None
         self._data_date = None
         self._data = client
-        self._hour = hour
         self._name = name
         self._sensor_type = sensor_type
 
-        if sensor_type == 'hour':
-            self._unique_id = f"{self._data.get_metering_point()}-{hour}"
-        else:
-            self._unique_id = f"{self._data.get_metering_point()}-total"
+        self._unique_id = f"{self._data.get_metering_point()}-total"
 
     @property
     def name(self):
@@ -59,6 +55,7 @@ class EloverblikEnergy(Entity):
         attributes = dict()
         attributes['Metering date'] = self._data_date
         attributes['metering_date'] = self._data_date
+        attributes['rawdata'] = self._rawdata
         
         return attributes
 
@@ -75,8 +72,14 @@ class EloverblikEnergy(Entity):
 
         self._data_date = self._data.get_data_date()
 
-        if self._sensor_type == 'total':
-            self._state = self._data.get_total_day()
-        else:
-            self._state = self._data.get_usage_hour(self._hour)
+        self._state = self._data.get_total_day()
+        self._rawdata = []
+        for x in range(1, 25):
+            hour_start = datetime.fromisoformat("{}T{:02d}:00:00".format(self._data_date, x-1))
+            hour_end = hour_start + timedelta(hours=1)
+            self._rawdata.append({
+                "start": f"{hour_start.isoformat()}",
+                "end": f"{hour_end.isoformat()}",
+                "value": self._data.get_usage_hour(x)
+                })
 
